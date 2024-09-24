6caeb533d0e787cce796f52bb499796b91733431
Hachi-24h <shinnichi987@gmail.com>
2024-09-24 13:41:29 +0700

    update thư mục

--- Screen/SignUp_Screen.js
+++ Screen/SignUp_Screen.js
@@ -1,29 +1,30 @@
 import React from 'react';
-import { View, Text, Image, TouchableOpacity, StyleSheet, ScrollView } from 'react-native';
-import styles from './Css/styles';
+import { View, Text, Image, TouchableOpacity, ScrollView } from 'react-native';
+import styles from '../Css/SignUp_css'; 
+
 const SignUp = () => {
   return (
     <ScrollView contentContainerStyle={styles.container}>
-      {/* Image grid */}
+
       <View style={styles.imageGrid}>
-        <Image style={styles.image} source={require('../Picture/image.png')} />
-        <Image style={styles.image} source={require('../Picture/image.png')} />
-        <Image style={styles.image} source={require('../Picture/image.png')} />
-        <Image style={styles.image} source={require('../Picture/image.png')} />
-        <Image style={styles.image} source={require('../Picture/image.png')} />
-        <Image style={styles.image} source={require('../Picture/image.png')} />
-        <Image style={styles.image} source={require('../Picture/image.png')} />
-        <Image style={styles.image} source={require('../Picture/image.png')} />
-        <Image style={styles.image} source={require('../Picture/image.png')} />
+        <Image style={styles.image} source={require('../Picture/image_1.png')} />
+        <Image style={styles.image} source={require('../Picture/image_1.png')} />
+        <Image style={styles.image} source={require('../Picture/image_1.png')} />
+        <Image style={styles.image} source={require('../Picture/image_1.png')} />
+        <Image style={styles.image} source={require('../Picture/image_1.png')} />
+        <Image style={styles.image} source={require('../Picture/image_1.png')} />
+        <Image style={styles.image} source={require('../Picture/image_1.png')} />
+        <Image style={styles.image} source={require('../Picture/image_1.png')} />
+        <Image style={styles.image} source={require('../Picture/image_1.png')} />
       </View>
 
-      {/* Logo and Welcome */}
+  
       <View style={styles.logoContainer}>
-        <Image style={styles.logo} source={require('./assets/image.png')} />
+        <Image style={styles.logo} source={require('../Picture/image_1.png')} /> 
         <Text style={styles.welcomeText}>Welcome to Pinterest</Text>
       </View>
 
-      {/* Buttons */}
+
       <View style={styles.buttonGroup}>
         <TouchableOpacity style={styles.signUpButton}>
           <Text style={styles.signUpText}>Sign up</Text>
@@ -33,11 +34,11 @@ const SignUp = () => {
         </TouchableOpacity>
       </View>
 
-      {/* Terms and Privacy */}
+      
       <View style={styles.termsContainer}>
-        <Text style={styles.termsText}>By continuing, you agree to Pinterest's</Text>
+        <Text style={styles.termsText}>By continuing, you agree to Pinterest&apos;s</Text>
         <Text style={styles.linkText}>Terms of Service</Text>
-        <Text style={styles.termsText}> and acknowledge you've read our </Text>
+        <Text style={styles.termsText}> and acknowledge you&apos;ve read our </Text>
         <Text style={styles.linkText}>Privacy Policy</Text>
       </View>
     </ScrollView>
